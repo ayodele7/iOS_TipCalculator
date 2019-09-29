da46eaf284859ba94b77fa6d4d1967e250ccf1e0
ayodele7 <ayodelefarinre@yahoo.com>
2019-09-29 18:37:23 -0400

    Basic Tip Calculator Layout

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -14,7 +14,39 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var tiplabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+        
+        print("Hello")
+        view.endEditing(true) 
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+      //Get Bill
+        let bill = Double (billField.text!) ?? 0
+        
+        //Calculate Tip
+        let percentages = [0.15,0.18,0.2]
+        
+        let tip = bill * percentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //Update Tip and total labels
+        tiplabel.text = String(format: "$%.2f",tip)
+        totalLabel.text = String(format: "$%.2f",total)
+        
+    }
+    
+    
+    
+    
+    
+    
 }
 
